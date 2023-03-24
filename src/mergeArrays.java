d5e3964f5833fe5eb033d42433d4806e4c07a440
Nikita Yalynsky <nikitos75091@gmail.com>
2023-03-24 17:38:55 +0900

    printTextPerRole

---
+++ src/mergeArrays.java
@@ -0,0 +1,14 @@
+public class mergeArrays {
+    public static int[] mergeArrays(int[] a1, int[] a2) {
+        int n = a1.length, m = a2.length;
+        int i = 0, j = 0;
+        int[] array = new int[n + m];
+
+        while (i < n || j < m)
+            array[i + j] = (i < n && (j == m || a1[i] < a2[j])) ? a1[i++] : a2[j++];
+
+        return array;
+    }
+}
+
+
